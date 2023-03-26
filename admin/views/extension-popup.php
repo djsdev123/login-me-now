447e9b10d6a0c00fb7dc123ac0f55a547a840c96
HeyMehedi <mehedi.sovware@gmail.com>
2023-03-26 11:33:30 +0600

    Update the save later button

--- admin/views/extension-popup.php
+++ admin/views/extension-popup.php
@@ -19,7 +19,30 @@ $html .= '</div>';
 echo $html;
 ?>
 <script>
+	let lmnLater = document.getElementById('lmn-later');
+	const keyUrlEmail = '<?php echo get_site_url() . '_' . $current_user->user_email; ?>';
+	let lmnExt = document.querySelector("#lmnExt");
 
+	if( typeof lmnLater !== 'undefined' && lmnLater	) {
+		lmnLater.addEventListener('click', function(e){
+			const today = new Date();
+			const nextWeek = new Date(today);
+			nextWeek.setDate(today.getDate() + 3);
+			localStorage.setItem(keyUrlEmail, nextWeek);
+			lmnExt.style.display = 'none';
+		})
+	}
+
+	let hasLater = localStorage.getItem(keyUrlEmail);
+	if( typeof hasLater !== 'undefined' && hasLater	) {
+		const today = new Date();
+		const expectedTime = new Date(hasLater);
+		if (today.getDate() !== expectedTime.getTime()) {
+			lmnExt.innerHTML = '';
+		} else {
+			localStorage.removeItem(keyUrlEmail);
+		}
+	}
 </script>
 
 <style>
@@ -30,12 +53,13 @@ echo $html;
 		width: 100%;
 		left: 0;
 		bottom: 0;
-		background: #000;
+		background: #263E94;
 		color: #fff;
 		text-align: center;
 		z-index: 999999999;
 		justify-content: center;
 		align-items: center;
+		transition: .3s;
 	}
 	#lmnExt button {
 		padding: 5px 15px;
@@ -45,7 +69,7 @@ echo $html;
 		box-shadow: 2px 2px 2px 2px #ffffff50;
 	}
 	#lmn-save {
-		background: #00ba00;
-		color: #fff;
+		background: #fff;
+		color: #000;
 	}
 </style>
